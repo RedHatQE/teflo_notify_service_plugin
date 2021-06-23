bb4c164d50216b55348e3178727d641d3df500a0
Rujuta Shinde <rushinde@redhat.com>
2021-06-23 12:21:35 -0400

    modified setup.py and pushtopypi

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
 import re
+import io
 from setuptools import setup, find_packages
 
 
@@ -12,10 +13,16 @@ def get_version():
     return VERSION_RE.search(init).group(1)
 
 
+# reading description from README.rst
+with io.open(os.path.join(ROOT, 'docs', 'index.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='teflo_notify_service_plugin',
     version=get_version(),
     description="Teflo plugin for integration with D&O tools's Notify Service ",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="Red Hat Inc",
     packages=find_packages(),
     include_package_data=True,
